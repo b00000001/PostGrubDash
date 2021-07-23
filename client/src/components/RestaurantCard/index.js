bdb55793e70c524fcb9c13afb96f9cfc830bc9e2
Justin Hui <huirayj@gmail.com>
2021-07-23 13:43:27 -0400

    created restaurant card

---
+++ client/src/components/RestaurantCard/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RestaurantCard = () => {
+    return (
+        <div className="">
+            <img src='https://res.cloudinary.com/grubhub/image/upload/d_search:browse-images:default.jpg/w_267,h_132,f_auto,q_auto,dpr_auto,g_auto,c_fill/kjgvrh5fckhubhkunqa1' alt='' />
+            <h3>Fuzzy's Taco Shop</h3>
+            <h4>Tacos</h4>
+            <span>35-45 mins</span>
+            <span>$5.99 delivery</span>
+            <span>5 stars</span>
+        </div>
+    )
+}
+
+export default RestaurantCard;
