74633e89026f4eb967f81e7b524859ee1c19e5e0
Justin Hui <huirayj@gmail.com>
2021-07-24 14:17:52 -0400

    updated models

---
+++ server/models/Restaurant.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const restaurantSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
+});
+
+const Restaurant = model('Restaurant', restaurantSchema);
+
+module.exports = Restaurant;
