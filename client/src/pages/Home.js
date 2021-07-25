e3e969a70a508cde963e2a725f999b576caa2ed6
Justin Hui <huirayj@gmail.com>
2021-07-25 18:48:48 -0400

    updated some styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,8 @@ import Auth from '../utils/auth';
 // import { QUERY_USERS } from '../utils/queries';
 // Components
 import UserList from '../components/UserList';
+import RestaurantCard from '../components/RestaurantCard';
+import SearchBar from '../components/SearchBar';
 
 const Home = () => {
   // const { loading, data } = useQuery(QUERY_USERS);
@@ -26,15 +28,25 @@ const Home = () => {
 
   return (
     <main>
-      <div className="flex justify-center">
-        <div
+      <div className="flex-col justify-center">
+        {/* <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
           {renderUsername()}
+        </div> */}
+        <SearchBar />
+        <div className="flex justify-center p-4">
+          <div className="grid grid-flow-row grid-cols-3 grid-rows-2 gap-10">
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+          </div>
         </div>
-        <div className="grid grid-flow-row grid-cols-3 grid-rows-3 gap-4">
-        </div>
+
       </div>
     </main>
   );
