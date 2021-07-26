1a13df7d1c58911a270b31e26385161082bfaa7e
Aaron Deas <deas.aaron@gmail.com>
2021-07-26 17:46:40 -0400

    Added comma to search field after getting location

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -12,7 +12,7 @@ export const SearchBar = ({ userLocation }) => {
             className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none"
             placeholder={
               userLocation
-                ? `${userLocation.lat} ${userLocation.lng}`
+                ? `${userLocation.lat}, ${userLocation.lng}`
                 : "Enter your delivery address"
             }
           />
