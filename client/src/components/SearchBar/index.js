49e419850a80fc8f1b0b2a8ea24dd3587e092aff
NZerr <nathan.zerr@gmail.com>
2021-07-26 14:07:39 -0400

    updated styling

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -4,7 +4,7 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export const SearchBar = () => {
     return (
-        <div className="bg-blue-400">
+        <div className="map-back">
             <div className="container h-96 flex justify-center items-center px-4 sm:px-6 lg:px-8">
                 <div className="relative">
                     <input type="text" className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="Enter your delivery address" />
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,9 +4,14 @@ const About = () => {
 
 return (
     <>
-    <div className="text-center">
-    <h1>About Us</h1>
-    <h1>Who We Are</h1>
+    <div className="text-center container">
+    <h1 className="text-4xl serif mb-5">About Us</h1>
+    <img className="max-w-xs inline-block" src="/images/delivery.jpg" alt="delivery person"/>
+    <p className="inline-block ml-8">Placeholder Text  </p>
+    <br/>
+    <br/>
+    <p className="inline-block">Placeholder Text</p>
+    <img className="inline-block max-w-xs" src="/images/customer.jpg" alt="customer" />
     </div>
     </>
 )
