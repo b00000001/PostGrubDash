1219d4dff7212c11d5bd4e7f1d9f977e9f573eb8
Aaron Deas <deas.aaron@gmail.com>
2021-07-26 17:45:58 -0400

    Added geolocator button that will populate the searchbar when pressed

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-export const SearchBar = () => {
-    return (
-        <div className="map-back">
-            <div className="container h-96 flex justify-center items-center px-4 sm:px-6 lg:px-8">
-                <div className="relative">
-                    <input type="text" className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="Enter your delivery address" />
-                    <div className="absolute top-4 right-3">
-                        <FontAwesomeIcon className="text-gray-400 z-20 hover:text-gray-500" icon={faSearch} />
-                    </div>
-                </div>
-            </div>
+export const SearchBar = ({ userLocation }) => {
+  return (
+    <div className="map-back">
+      <div className="container h-96 flex justify-center items-center px-4 sm:px-6 lg:px-8">
+        <div className="relative">
+          <input
+            type="text"
+            className="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none"
+            placeholder={
+              userLocation
+                ? `${userLocation.lat} ${userLocation.lng}`
+                : "Enter your delivery address"
+            }
+          />
+          <div className="absolute top-4 right-3">
+            <FontAwesomeIcon
+              className="text-gray-400 z-20 hover:text-gray-500"
+              icon={faSearch}
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
