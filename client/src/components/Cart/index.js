97c3f7c79b3ac20c64fabae64bf57220f01509d8
Justin Hui <huirayj@gmail.com>
2021-07-27 12:31:12 -0400

    enabled stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-// import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
@@ -9,19 +9,19 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
-// const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
     const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
-    //   useEffect(() => {
-    //     if (data) {
-    //       stripePromise.then((res) => {
-    //         res.redirectToCheckout({ sessionId: data.checkout.session });
-    //       });
-    //     }
-    //   }, [data]);
+      useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+      }, [data]);
 
     useEffect(() => {
         async function getCart() {
