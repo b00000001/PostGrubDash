dcc1dd7e21f22e9354119373e02f21d0f3b3949c
Aaron Deas <deas.aaron@gmail.com>
2021-07-27 20:17:10 -0400

    Added location search to APP, will return a distance value based on lng/lat coordinates provided to it

---
+++ client/src/components/Location/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  DistanceMatrixService,
+  GoogleMap,
+  useLoadScript
+} from "@react-google-maps/api";
+const Location = ({ userLocation }) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  });
+  console.log(loadError);
+  if (!isLoaded) return <h1> Loading...</h1>;
+
+  return (
+    <div>
+      <GoogleMap>
+        <DistanceMatrixService
+          options={{
+            //
+            destinations: [{ lat: 35.227229, lng: -80.843069 }],
+            origins: [{ lng: userLocation.lng, lat: userLocation.lat }],
+            travelMode: "DRIVING"
+          }}
+          callback={(response) => {
+            console.log(response);
+          }}
+        />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Location;
